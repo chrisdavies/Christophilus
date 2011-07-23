8e5354f56f6e7ecc3e26f8ac2f772709b77e7aa4
Christopher Davies <ferociousturtle@gmail.com>
2011-07-23 02:55:25 -0400

    Turning off the error filter so remote errors will show up.

--- Christophilus/Global.asax.cs
+++ Christophilus/Global.asax.cs
@@ -17,7 +17,6 @@
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
         }
 
         public static void RegisterRoutes(RouteCollection routes)
