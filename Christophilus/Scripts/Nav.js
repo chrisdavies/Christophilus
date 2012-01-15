328b869ed2bd83b69421f8e96b953a8b2b3e5785
Christopher Davies <ferociousturtle@gmail.com>
2012-01-14 19:37:36 -0500

    Adding global nav, and a link for the tag-cloud feature (tbd).

--- Christophilus/Scripts/Nav.js
+++ Christophilus/Scripts/Nav.js
@@ -1,10 +1,5 @@
-﻿toph.Nav = {
-    init: function () {
-        toph.Nav.initLogoutBehavior();
-        toph.Nav.initFadeBehavior();
-    },
-
-    initLogoutBehavior: function () {
+﻿$(function () {
+    function initLogoutBehavior() {
         var logoutLink = $('#logout-link');
         var userOptions = $('#user-options');
         logoutLink.css('width', userOptions.width() + 'px');
@@ -13,13 +8,12 @@
             logoutLink.toggle();
             return false;
         });
-    },
+    }
 
-    initFadeBehavior: function () {
+    function initFadeBehavior() {
         var nav = $('#main-navigation');
         nav.fadeTo('slow', .3);
-        nav.hover(
-        function hoverIn() {
+        nav.hover(function hoverIn() {
             nav.stop();
             nav.fadeTo('fast', 1);
         },
@@ -28,6 +22,7 @@
             nav.fadeTo('slow', .3);
         });
     }
-}
 
-$(document).ready(toph.Nav.init);
+    initLogoutBehavior();
+    initFadeBehavior();
+});
